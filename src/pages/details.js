e3e02dd607ebd8beef34e8577e76ab63a5628f28
emilyvyha120 <emilyvyha120@tamu.edu>
2023-04-17 17:06:17 -0500

    fixed details page centering

--- src/pages/details.js
+++ src/pages/details.js
@@ -15,18 +15,18 @@ extends React.Component {
         return (
         <div style = {{position: "absolute", right: "0px", left: "0px", top: "0px", bottom: "0px", backgroundColor: "white", }}>
             <h3 style = {{fontSize: "5vh", textAlign: "center", margin: "2vh", color: "#F0080A"}}> Insert Movie Title Here </h3>
-            <div style = {{marginLeft: "auto", marginRight: "auto", width: "100vh"}}>
+            <div style = {{display: "flex", justifyContent: "center", marginTop: "2vh"}}>
                 <YouTube videoId="Md6Dvxdr0AQ" 
                     opts={opts} onReady={this._onReady} />
             </div>
-            <div style = {{fontSize: "3vh", marginTop: "2vh", width: "100vh", marginLeft: "60vh"}}>
-                <p>
+            <div style = {{fontSize: "3vh", marginTop: "2vh", textAlign: "center", width: "100vh", marginLeft: "60vh"}}>
+                <p style = {{textAlign: "left"}}>
                     Insert genre
                 </p>
-                <p>
+                <p style = {{textAlign: "left"}}>
                     Insert rating
                 </p>
-                <p>
+                <p style = {{textAlign: "left"}}>
                     Insert description
                 </p>
             </div>
