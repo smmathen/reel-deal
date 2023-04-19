bba01881940ebf136ca947674db961ba1bf79dcd
t2tran <15519+t2tran@users.noreply.github.tamu.edu>
2023-04-19 16:07:01 -0500

    unable to fix yt link, checking shawn's branch

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,9 +1,13 @@
-import React from "react";
+import React, {useState, useEffect } from 'react';
 import YouTube from "react-youtube";
   
 
+function updateVideoId(newVideoId) {
+    setVideoId(newVideoId);
+  }
+
 function Details({ index, movies }){
-    const { Title: title, Genre: genre, Rating: rating, Description: description, Video_ID: videoId } = movies[index];
+    const { Title: title, Trailer_Link: trailer_link ,Genre: genre, Rating: rating, Description: description} = movies[index];
     const opts = {
         height: "400vh",
         width: "700vh",
@@ -12,18 +16,22 @@ function Details({ index, movies }){
         },
     };
 
+    useEffect(() => {
+        console.log(trailer_link);
+      }, [index, movies]);
+
     const _onReady = (event) => {
         event.target.pauseVideo();
     }
 
     return(
         <div>
-            <h3 style = {{fontSize: "5vh", textAlign: "center", margin: "2vh", color: "#F0080A"}}> Insert Movie Title Here </h3>
-            <div style = {{marginLeft: "auto", marginRight: "auto", width: "100vh"}}>
-            <YouTube videoId={videoId} opts={opts} onReady={_onReady} />
+            <h3 style = {{fontSize: "5vh", textAlign: "center", margin: "2vh", color: "#F0080A"}}>{title}</h3>
+            <div style = {{width: "20%"}}>
+                <YouTube videoId={trailer_link} opts={opts} onReady={_onReady}/>
             </div>
             <div style={{ fontSize: "3vh", marginTop: "2vh", width: "100vh", marginLeft: "60vh" }}>
-                <p>{genre}</p>
+                <p>{trailer_link}</p>
                 <p>{rating}</p>
                 <p>{description}</p>
             </div>
