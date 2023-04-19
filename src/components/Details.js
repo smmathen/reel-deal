bc65e26812e5c1160a715d0da8cf4410f6609f5e
t2tran <15519+t2tran@users.noreply.github.tamu.edu>
2023-04-19 16:40:37 -0500

    details synced with main

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,8 +1,8 @@
-import React, {useState, useEffect } from 'react';
+import React from "react";
 import YouTube from "react-youtube";
 
 
-function Details({ index, movies }){
+function Details({ index, movies }) {
     const { Title: title, Genre: genre, Rating: rating, Description: description, Trailer_Link: videoId } = movies[index];
     const opts = {
         height: "300vh",
@@ -12,21 +12,17 @@ function Details({ index, movies }){
         },
     };
 
-    useEffect(() => {
-        console.log(trailer_link);
-      }, [index, movies]);
-
     const _onReady = (event) => {
         event.target.pauseVideo();
     }
     return (
         <div>
-            <h3 style = {{fontSize: "5vh", textAlign: "center", margin: "2vh", color: "#F0080A"}}> Insert Movie Title Here </h3>
-            <div style = {{marginLeft: "auto", marginRight: "auto", width: "100vh"}}>
-            <YouTube videoId={videoId} opts={opts} onReady={_onReady} />
+            <h3 style={{ fontSize: "5vh", textAlign: "center", margin: "2vh", color: "#F0080A" }}> {title} </h3>
+            <div style={{ marginLeft: "auto", marginRight: "auto", width: "100vh" }}>
+                <YouTube videoId={videoId} opts={opts} onReady={_onReady} />
             </div>
             <div style={{ fontSize: "3vh", marginTop: "2vh", width: "100vh", marginLeft: "60vh" }}>
-                <p>{trailer_link}</p>
+                <p>{genre}</p>
                 <p>{rating}</p>
                 <p>{description}</p>
             </div>
