0f9854c46f0b44d70b57d96b3f8e6bf54869e309
ethannov <ethannovicio@gmail.com>
2023-04-24 16:18:36 -0500

    name flex box

--- src/pages/joinSession.js
+++ src/pages/joinSession.js
@@ -51,17 +51,19 @@ export default function Session() {
                     </p>
                 </div>
             </div>
-            <div style={{ position: "relative", top: "10%" }}>
-                <p style={{ fontSize: "5vh" }}>
-                    Movie Watchers:
-                </p>
-                <ul style={{ fontSize: "4vh" }}>
-                    {users.map((user) => (
-                        <li key={user}>{user}</li>
-                    ))}
-                </ul>
-                <button onClick={handleStart}>Start</button>
+            <div style={{ position: "relative", top: "10%", textAlign: "center", margin: "auto" }}>
+            <p style={{ fontSize: "5vh" }}>Movie Watchers:</p>
+            <ul style={{ fontSize: "4vh", display: "flex", flexWrap: "wrap", minwidth: "80%" }}>
+                {users.map((user) => (
+                <li key={user} style={{ flexBasis: "25%", minWidth: "100px" }}>
+                    {user}
+                </li>
+                ))}
+            </ul>
+
+            <button onClick={handleStart}>Start</button>
             </div>
+
         </div>
     )
 }
